6ebfbd621640af03b044e21b0727898c8e45be05
MLotfy88 <m.m.lotfy.88@gmail.com>
2025-10-20 02:12:24 +0300

    Changes to be committed: 	deleted:    Screenshot_20251019-160212.png 	modified:   src/components/layouts/manager-layout.tsx 	modified:   src/components/layouts/supervisor-layout.tsx

--- src/components/layouts/manager-layout.tsx
+++ src/components/layouts/manager-layout.tsx
@@ -69,7 +69,7 @@ const ManagerLayout: React.FC = () => {
           </SidebarMenu>
         </SidebarContent>
       </Sidebar>
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
         <Header />
         <main className={isEvaluatePage ? "flex-1" : "flex-1 p-4 sm:p-6 md:p-8"}>
           <div className={isEvaluatePage ? "" : "max-w-7xl mx-auto"}>
--- src/components/layouts/supervisor-layout.tsx
+++ src/components/layouts/supervisor-layout.tsx
@@ -44,7 +44,7 @@ const SupervisorLayout: React.FC = () => {
           </SidebarMenu>
         </SidebarContent>
       </Sidebar>
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col min-w-0">
         <Header />
         <main className="flex-1 p-4 sm:p-6 md:p-8">
           <div className="max-w-7xl mx-auto">
