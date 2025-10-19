89d74fdff37f4d752e0c3caa1885acedd63e2984
MLotfy88 <m.m.lotfy.88@gmail.com>
2025-10-19 15:32:04 +0300

    Changes to be committed: 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,48 @@ const LoadingFallback = () => (
   </div>
 );
 
+// This new component will contain all routes that need the sidebar.
+const ProtectedRoutesWithSidebar = () => (
+  <SidebarProvider>
+    <Routes>
+      {/* Protected Routes */}
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Index />} />
+        
+      {/* Supervisor Routes */}
+      <Route element={<RoleBasedRoute allowedRoles={['supervisor']} />}>
+        <Route element={<SupervisorLayout />}>
+          <Route path="/supervisor/dashboard" element={<SupervisorDashboard />} />
+          <Route path="/supervisor/evaluate" element={<Evaluate />} />
+          <Route path="/supervisor/history" element={<SupervisorHistory />} />
+        </Route>
+      </Route>
+
+      {/* Manager Routes */}
+      <Route element={<RoleBasedRoute allowedRoles={['manager']} />}>
+        <Route element={<ManagerLayout />}>
+          <Route path="/manager/dashboard" element={<ManagerDashboard />} />
+          <Route path="/manager/nurses" element={<NursesPage />} />
+          <Route path="/manager/nurses/:id" element={<NurseProfilePage />} />
+          <Route path="/manager/audits" element={<AuditsPage />} />
+          <Route path="/manager/supervisors" element={<SupervisorsPage />} />
+          <Route path="/manager/reports" element={<ReportsPage />} />
+          <Route path="/manager/badges" element={<BadgesPage />} />
+          <Route path="/manager/leaderboard" element={<LeaderboardPage />} />
+          <Route path="/all-badges" element={<AllBadgesPage />} />
+          <Route path="/manager/evaluate" element={<Evaluate />} />
+          <Route path="/manager/history" element={<SupervisorHistory />} />
+        </Route>
+      </Route>
+      </Route>
+
+      {/* Catch-all Route */}
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </SidebarProvider>
+);
+
+
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
@@ -42,47 +84,15 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <AuthProvider>
-          <SidebarProvider>
-            <Suspense fallback={<LoadingFallback />}>
-              <Routes>
-                <Route path="/login" element={<Login />} />
-                
-                {/* Protected Routes */}
-                <Route element={<ProtectedRoute />}>
-                  <Route path="/" element={<Index />} />
-                  
-                {/* Supervisor Routes */}
-                <Route element={<RoleBasedRoute allowedRoles={['supervisor']} />}>
-                  <Route element={<SupervisorLayout />}>
-                    <Route path="/supervisor/dashboard" element={<SupervisorDashboard />} />
-                    <Route path="/supervisor/evaluate" element={<Evaluate />} />
-                    <Route path="/supervisor/history" element={<SupervisorHistory />} />
-                  </Route>
-                </Route>
-
-                {/* Manager Routes */}
-                <Route element={<RoleBasedRoute allowedRoles={['manager']} />}>
-                  <Route element={<ManagerLayout />}>
-                    <Route path="/manager/dashboard" element={<ManagerDashboard />} />
-                    <Route path="/manager/nurses" element={<NursesPage />} />
-                    <Route path="/manager/nurses/:id" element={<NurseProfilePage />} />
-                    <Route path="/manager/audits" element={<AuditsPage />} />
-                    <Route path="/manager/supervisors" element={<SupervisorsPage />} />
-                    <Route path="/manager/reports" element={<ReportsPage />} />
-                    <Route path="/manager/badges" element={<BadgesPage />} />
-                    <Route path="/manager/leaderboard" element={<LeaderboardPage />} />
-                    <Route path="/all-badges" element={<AllBadgesPage />} />
-                    <Route path="/manager/evaluate" element={<Evaluate />} />
-                    <Route path="/manager/history" element={<SupervisorHistory />} />
-                  </Route>
-                </Route>
-                </Route>
-
-                {/* Catch-all Route */}
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </Suspense>
-          </SidebarProvider>
+          <Suspense fallback={<LoadingFallback />}>
+            <Routes>
+              {/* Public route without sidebar */}
+              <Route path="/login" element={<Login />} />
+              
+              {/* All other routes are nested here and will have the sidebar */}
+              <Route path="/*" element={<ProtectedRoutesWithSidebar />} />
+            </Routes>
+          </Suspense>
         </AuthProvider>
       </BrowserRouter>
     </TooltipProvider>
