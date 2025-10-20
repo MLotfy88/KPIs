87efd54c6cdc8644b56de7fba26c5b9cc84c6234
MLotfy88 <m.m.lotfy.88@gmail.com>
2025-10-20 10:47:18 +0300

    Changes to be committed: 	modified:   src/components/layouts/manager-layout.tsx 	modified:   src/components/layouts/supervisor-layout.tsx

--- src/components/layouts/manager-layout.tsx
+++ src/components/layouts/manager-layout.tsx
@@ -67,10 +67,10 @@ const ManagerLayout: React.FC = () => {
           </SidebarMenu>
         </SidebarContent>
       </Sidebar>
-      <div className="flex-1 flex flex-col min-w-0 w-full overflow-x-hidden">
+      <div className="flex-1 flex flex-col min-w-0">
         <Header />
-        <main className="flex-1 p-4 sm:p-6 md:p-8">
-          <div className="max-w-7xl mx-auto w-full">
+        <main className="flex-1 flex flex-col p-4 sm:p-6 md:p-8">
+          <div className="max-w-7xl mx-auto w-full flex-grow flex">
             <Outlet />
           </div>
         </main>
